8c21533605f2e682f8498a1474d08d1dc0ef3aaf
NG WEI HEN <rolandng0001@gmail.com>
2024-03-16 01:15:14 +0800

    Update for UI

--- src/View/Doctor/Profile/DoctorProfile.jsx
+++ src/View/Doctor/Profile/DoctorProfile.jsx
@@ -6,8 +6,8 @@ const DoctorProfile = () => {
     <div>
       <title>BITU3973 | Doctor Profile</title>
       <DoctorLayout>
-        <div className="container mx-auto p-4">
-          <h1 className="text-2xl font-bold mb-4">Employee Data</h1>
+        <div className="container p-4">
+          <h1 className="text-2xl font-bold mb-4 text-white">User Profile</h1>
           
         </div>
       </DoctorLayout>
