bb0b3f492ca3f1f9cc08249bd73a2e7c8b76beef
NG WEI HEN <rolandng0001@gmail.com>
2024-03-24 16:13:53 +0800

    update UI

--- src/View/Forget Password.jsx
+++ src/View/Forget Password.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const ForgetPassword = () => {
+
+  const bodyStyle = {
+    background: 'linear-gradient(to right, #301847, #C10214)', // Set your desired background color here // Ensure the gradient covers the entire viewport height
+    display: 'flex',
+    color: '#fff', // Set text color to contrast with the gradient
+    fontSize: '24px',
+    minHeight: '100vh', // Ensure the div covers the full viewport height
+    overflowY: 'auto', // Add vertical scrollbar if content overflows
+  };
+
+  const shadowStyle = {
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', // Horizontal offset, Vertical offset, Blur radius, Color
+  };
+
   return (
-    <div>
+    <div style = {bodyStyle} className ="grid grid-cols-2 px-4 items-center h-screen">
       <title>BITU3973 | Forget Password</title>
       Forget Password ABC
     </div>
