305b1e47d6d765f5e3d0cd6b4dbe828c14a8c820
NG WEI HEN <rolandng0001@gmail.com>
2024-03-28 18:51:07 +0800

    Update UI

--- src/View/Admin/Pages/AdminHome.jsx
+++ src/View/Admin/Pages/AdminHome.jsx
@@ -12,7 +12,10 @@ const AdminHome = () => {
     <div style={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
       <title>BITU3973 | Admin Home</title>
       <AdminLayout adminID={adminID} adminName={adminName}>
-        
+        {/** Row 1 */}
+        <div class="w-full px-5 pt-3 h-fit overflow-hidden ">
+          <h1 class="text-xl"><b>Dashboard</b></h1>
+        </div>
       </AdminLayout>
     </div>
   )
--- src/View/Admin/Pages/Control User/Admin Control User.jsx
+++ src/View/Admin/Pages/Control User/Admin Control User.jsx
@@ -11,8 +11,20 @@ const AdminControlUser = () => {
     <div>
         <title>BITU3973 | Admin User Activity Log</title>
         <AdminLayout adminID={adminID} adminName={adminName}>
+          
+          {/** Row 1 */}
+          <div class="w-full px-5 pt-3 h-fit overflow-hidden">
+            <h1 class="text-xl"><b>User Activity Log</b></h1>
+          </div>
+
+           {/** Row 2 */}
           <div class="w-full px-5 pt-3 h-fit overflow-hidden ">
-            <h1 class="text-xl"><b>Dashboard</b></h1>
+            <h1 class="text-xl"><b>Patient</b></h1>
+          </div>
+
+          {/** Row 3 */}
+          <div class="w-full px-5 pt-3 h-fit overflow-hidden ">
+            <h1 class="text-xl"><b>Doctor</b></h1>
           </div>
         </AdminLayout>
     </div>
