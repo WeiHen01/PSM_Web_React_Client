a33ab304c889efe48a7941406c7bf7a5347a332d
NG WEI HEN <rolandng0001@gmail.com>
2024-04-03 09:48:58 +0800

    Update UI

--- src/View/Admin/Components/AdminHeader.jsx
+++ src/View/Admin/Components/AdminHeader.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef  }  from 'react';
 import Trademark from "../../../images/Trademark_color.png";
 import Logo from "../../../images/Logo.png";
 import { useNavigate } from 'react-router-dom';
-import {Bell, LogOut, Settings, UserCircle2} from 'lucide-react';
+import {Bell, LogOut, UserCircle2} from 'lucide-react';
 
 const AdminHeader = ({ adminID, adminName, notificationCount, children }) => {
 
